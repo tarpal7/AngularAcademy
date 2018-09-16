b7f7e48a5b1c8100bcc1aa2ccc6010e2ca7c0e3c
Pavlo <tarpal7@gmail.com>
2018-09-16 12:25:31 +0300

    HomeTask2:  created component Galery

---
+++ src/app/galery/galery.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-galery',
+  templateUrl: './galery.component.html',
+  styleUrls: ['./galery.component.css']
+})
+export class GaleryComponent {
+  @Input() item
+}
